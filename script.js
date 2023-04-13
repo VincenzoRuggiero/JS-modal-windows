430176e0c52642081780613f7cdd7b2b77e5c91e
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-13 17:03:42 +0200

    Add feat to clase a modal by pressing "Esc" on the keyboard

--- script.js
+++ script.js
@@ -22,3 +22,10 @@ for (let i = 0; i < btnsOpenModal.length; i++) {
 
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
+
+// Closing modal with Esc key
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  }
+});
