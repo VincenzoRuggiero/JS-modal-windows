3dafd8ea011112f16039ff8b200779ee834e75db
Vincenzo Ruggiero <45271214+VincenzoRuggiero@users.noreply.github.com>
2023-04-13 16:45:13 +0200

    Refactoring using functions

--- script.js
+++ script.js
@@ -5,15 +5,20 @@ const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnsOpenModal = document.querySelectorAll(".show-modal");
 
-for (let i = 0; i < btnsOpenModal.length; i++) {
-  btnsOpenModal[i].addEventListener("click", function () {
-    modal.classList.remove("hidden");
-    overlay.classList.remove("hidden");
-    console.log("button clicked");
-  });
-}
-
-btnCloseModal.addEventListener("click", function () {
+// Functions to open & close modals
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = function () {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
-});
+};
+
+// All modals' buttons show the modal + overlay
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
